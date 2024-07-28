16ae3bc144dff959e0044e7f62ae828e2feca2bd
Sipos Eduard <sipos.eduard1@gmail.com>
2024-07-28 14:55:14 +0300

    Add openzeplin

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract CounterScript is Script {
     Counter public counter;
 
@@ -15,5 +17,12 @@ contract CounterScript is Script {
         counter = new Counter();
 
         vm.stopBroadcast();
+    }
+}
+
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply);
     }
 }
